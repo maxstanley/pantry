473737bcc53d8d28eaf5571cf3f7a32008b9e18d
Max Stanley <github@maxstanley.uk>
2021-03-24 19:59:02 +0000

    Issue #3: Create Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 export default function App(): JSX.Element {
 	return (
 		<div className="App">
-			<p>Welcome to Pantry!</p>
+			<HashRouter basename="/">
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</HashRouter>
 		</div>
 	);
 }
